65dbba51e8eaff06d0edef30c3a062411aa290f6
luca-poddigue <luca.poddigue@gmail.com>
2019-09-21 14:25:44 +0200

    Adding v1 to base path

--- src/main/java/org/acme/quickstart/GreetingResource.java
+++ src/main/java/org/acme/quickstart/GreetingResource.java
@@ -10,7 +10,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("v1/hello")
 public class GreetingResource {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GreetingResource.class);
